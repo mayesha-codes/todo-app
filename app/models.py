d518da40287b1236ea9c149b57a50022fde940e8
mayesha-codes <maisha.tasnim.247@gmail.com>
2025-02-16 16:04:53 +0600

    Migrate project from Flask to FastAPI - Removed old Flask codebase

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class User(db.Model):
-    user_id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(45),nullable=False)
-    email=db.Column(db.String(45),unique=True,nullable=False)
-    password=db.Column(db.String(45),nullable=False)
-    role=db.Column(db.String(45),nullable=False)
--- config.py
+++
@@ -1,5 +0,0 @@
-DATABSE_URI="mysql+pymysql://root:root@localhost/todo_db"
-
-class Config:
-    SQLALCHEMY_DATABASE_URI=DATABSE_URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
