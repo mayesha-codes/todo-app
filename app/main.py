02d49f482aff55d69f901d5480f442aa82d0177a
mayesha-codes <maisha.tasnim.247@gmail.com>
2025-02-20 01:27:22 +0600

    router integration to main.py file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import users, todos
+from .routers import users, todos,sub_todos
 from . import models
 from .database import  engine
 models.Base.metadata.create_all(bind=engine)
@@ -10,6 +10,7 @@ app = FastAPI()
 #Include routers
 app.include_router(users.router)
 app.include_router(todos.router)
+app.include_router(sub_todos.router)
 
 @app.get("/")
 def home():
